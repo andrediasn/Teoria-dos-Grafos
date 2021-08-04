f7d60c56cab32e4359067c051a5669d5bbac244d
andrediasn <andredias@ice.ufjf.br>
2021-08-04 11:39:37 -0300

    logica do dijkstra

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,5 +1,6 @@
-/*#include "Dijkstra.h"
+#include "Dijkstra.h"
 #include "Grafo.h"
+#include "Vertices.h"
 #include <vector>
 #include <iostream>
 #include <limits.h>
@@ -8,69 +9,70 @@ using namespace std;
 
 
 
-int Dijkstra::dijkstra(Grafo *grafo, int noI){
-    int *dist = (int *)malloc(grafo->ordem*sizeof(int)); // aloca espaço pra array de distancias
-
-    int noP[grafo->ordem]; // aloca espaço para os predecessores
+int Dijkstra::dijkstra(Grafo *grafo, int noI, int noAlvo){
     
-    for (int i=0; i<grafo->ordem; i++) { // inicializa todos nos com distancia = infinito
-        dist[i] = INT_MAX/2; // Maior inteiro/2, pois maior inteiro +1 é negativo
-        noP[i] = -1; // sem predecessor
-    }
-    dist[noI] = 0; // Distancia com ele mesmo
+    int dist[grafo->getOrdem()]; // Vetor de distancia
+    int noP[grafo->getOrdem()];  // Armazena predecessor
 
-    for(auto i = nosGrafo.begin(); i != nosGrafo.end(); i++){
+    for(auto i = grafo->nosGrafo.begin(); i != grafo->nosGrafo.end(); i++){
         Vertices* aux = *i;
-        aux->setVisitado(false);
+        aux->setVisitado(true); // Aberto/Fechado
+        int id = aux->getId();
+        dist[id] = INT_MAX/2;
+        noP[id] = -1; // sem predecessor
     }
+    dist[noI] = 0;
     
-    while (verOpen(grafo, open)) {
-        int no1 = menorDist(grafo, open, dist);
-        open[no1] = false;
+    while (verOpen(grafo)) {
+        Vertices* no1 = menorDist(grafo, dist);
+        no1->setVisitado(false);
    
-        ADJACENCIA *ad = grafo->adj[no1].cab;
-        while (ad) {
-            atualizaDist(grafo, dist, noP, no1, ad->vertice);
-            ad = ad->prox;
+       for (auto i = no1->ListAdj.begin(); i != no1->ListAdj.end(); i++) {
+            Vertices* no2 = grafo->procurarNo(*i);
+            atualizaDist(grafo, dist, noP, no1, no2);
         }
     }
-    return(dist);
+    return(dist[noAlvo]);
 }
 
-void Dijkstra::atualizaDist(Grafo *grafo, int *dist, int *noP, int no1, int no2) {
-    ADJACENCIA *ad = grafo->adj[no1].cab;
-
-    while (ad && ad->vertice != no2)
-        ad = ad->prox;
-    if(ad){
-        if (dist[no2] > dist[no1] + ad->peso) { // dist de no2 maior que a soma da dist de no1 mais o peso da aresta
-            dist[no2] = dist[no1] + ad->peso; // atualiza dist de no2
-            noP[no2] = no1; // indica o predecessor
+void Dijkstra::atualizaDist(Grafo *grafo, int *dist, int *noP, Vertices* no1, Vertices* no2) {
+    for (auto i = no1->ListAdj.begin(); i != no1->ListAdj.end(); i++) {  
+        Vertices* aux = grafo->procurarNo(*i);
+        if(aux == no2){
+            if (dist[no2->getId()] > (dist[no1->getId()] + aresta->peso)) { // dist de no2 maior que a soma da dist de no1 mais o peso da aresta
+                dist[no2->getId()] = dist[no1->getId()] + aresta->peso; // atualiza dist de no2
+                noP[no2->getId()] = no1->getId(); // indica o predecessor
+            }
+            break;
         }
     }
 }
 
 bool Dijkstra::verOpen(Grafo *grafo) {
-    for(int i=0; i < grafo->ordem; i++) {
+    for(auto i = grafo->nosGrafo.begin(); i != grafo->nosGrafo.end(); i++){
         Vertices* aux = *i;
         if(aux->getVisitado())
             return true;
-        else
-            return false;
     }
+    return false;
 }
 
-int Dijkstra::menorDist(Grafo *grafo, bool *open, int *dist) {
-    int i;
-    for(i=0; i<grafo->ordem; i++)
-        if(open[i])  // busca primeiro vertice aberto
+Vertices* Dijkstra::menorDist(Grafo *grafo, int *dist) {
+    Vertices* aux;
+    for(auto i = grafo->nosGrafo.begin(); i != grafo->nosGrafo.end(); i++){
+        aux = *i;
+        if(aux->getVisitado())  // busca primeiro vertice aberto
             break;   // interrompe for ao achar
-    if(i == grafo->ordem)   // se após busca não encontrou vertice aberto
-        return(-1); 
-    int menor = i; // se achou, assumimos i como sendo a menor distancia
-    for (i=menor+1; i<grafo->ordem; i++) // continua a busca por outro vertice aberto
-        if(open[i] && (dist[menor]>dist[i]))   // se encontra outro aberto e a dist do atual menor for maior que o novo vertice aberto
-            menor = i; // atualiza menor
+    }
+    if(aux == grafo->nosGrafo.end())   // se após busca não encontrou vertice aberto
+        return(aux); 
+
+    Vertices* menor = aux; // se achou, assumimos i como sendo a menor distancia
+    for (auto i = menor++; i != grafo->nosGrafo.end(); i++) {// continua a busca por outro vertice aberto
+        aux = *i;
+        if(aux->getVisitado() && (dist[menor->getId()]>dist[aux->getId()]))   // se encontra outro aberto e a dist do atual menor for maior que o novo vertice aberto
+            menor = aux; // atualiza menor
+    }
     return(menor); // retorna o vertice com menor distancia
-}*/
+}
 
--- Dijkstra.h
+++ Dijkstra.h
@@ -1,16 +1,17 @@
-/*#ifndef GRAFOS2_DIJKSTRA_H
+#ifndef GRAFOS2_DIJKSTRA_H
 #define GRAFOS2_DIJKSTRA_H
 #include "Grafo.h"
+#include "Vertices.h"
 #include <vector>
 
 class Dijkstra {
 public:
-    int *dijkstra(Grafo *grafo, int noI);
+    int dijkstra(Grafo *grafo, int noI, int noAlvo);
     
 private:
-    void atualizaDist(Grafo *grafo, int *dist, int *noP, int no1, int no2);
-    bool verOpen(Grafo *grafo, bool *open);
-    int menorDist(Grafo *grafo, bool *open, int *dist);
+    void atualizaDist(Grafo *grafo, int *dist, int *noP, Vertices* no1, Vertices* no2);
+    bool verOpen(Grafo *grafo);
+    Vertices* menorDist(Grafo *grafo, int *dist);
 };
 
 #endif //GRAFOS2_DIJKSTRA_H*/
